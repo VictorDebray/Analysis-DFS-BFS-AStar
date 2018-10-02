58c77ea0d8ffb1f9b42e9e6ab0bdc0a5692c610a
debray_v <victor.debray@epitech.eu>
2018-10-01 21:40:13 -0400

    clean optimized

--- Node.py
+++ Node.py
@@ -33,41 +33,6 @@ class Node:
                 node = Node(new_puzzle, self.width, self.height)
                 self.nodes.append(node)
 
-
-        new_puzzle = movements.move_up_right(self.puzzle, self.empty_tile_index, self.width, self.height)
-        if new_puzzle is not None:
-            self.up_right = Node(new_puzzle, self.width, self.height)
-
-        new_puzzle = movements.move_right(self.puzzle, self.empty_tile_index, self.width)
-        if new_puzzle is not None:
-            self.right = Node(new_puzzle, self.width, self.height)
-
-        new_puzzle = movements.move_down_right(self.puzzle, self.empty_tile_index, self.width, self.height)
-        if new_puzzle is not None:
-            self.down_right = Node(new_puzzle, self.width, self.height)
-
-        new_puzzle = movements.move_down(self.puzzle, self.empty_tile_index, self.width, self.height)
-        if new_puzzle is not None:
-            self.down = Node(new_puzzle, self.width, self.height)
-
-        new_puzzle = movements.move_down_left(self.puzzle, self.empty_tile_index, self.width, self.height)
-        if new_puzzle is not None:
-            self.down_left = Node(new_puzzle, self.width, self.height)
-
-        new_puzzle = movements.move_left(self.puzzle, self.empty_tile_index, self.width)
-        if new_puzzle is not None:
-            self.left = Node(new_puzzle, self.width, self.height)
-
-        new_puzzle = movements.move_up_left(self.puzzle, self.empty_tile_index, self.width, self.height)
-        if new_puzzle is not None:
-            self.up_left = Node(new_puzzle, self.width, self.height)
-
     def print_node(self):
-        print("Up move" + self.up.puzzle)
-        print("Up right move" + self.up_right.puzzle)
-        print("Right right move" + self.right.puzzle)
-        print("Down right move" + self.down_right.puzzle)
-        print("Down move" + self.down.puzzle)
-        print("Down left move" + self.down_left.puzzle)
-        print("Left move" + self.left.puzzle)
-        print("Up Left move" + self.up_left.puzzle)
+        for node in self.nodes:
+            print(node.puzzle)
--- utils.py
+++ utils.py
@@ -1,11 +1,24 @@
 def is_goal(puzzle):
     return puzzle == [(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'), (5, 'e'), (6, 'f'), (7, 'g'), (8, 'h'), (9, 'i'), (10, 'j'), (11, 'k'), (0, 'l')]
 
-def clean(node, open_states, closed_states):
-    for i, item in enumerate(node.nodes):
-        for it in open_states:
-            if it.state == item.state:
-                del node.nodes[i]
+def clean(node, open_states, closed_states, open_list):
+    id_node_tup = [(x.id, x) for x in node.nodes]
+    for item in id_node_tup:
+        if item[0] in open_states:
+            node.nodes.remove(item[1])
+            break
         for it in closed_states:
-            if it.state == item.state:
-                del node.nodes[i]
+            if it.id == item[0]:
+                node.nodes.remove(item[1])
+
+# def clean(node, open_states, closed_states):
+#     id_node_tup = [(x.id, x) for x in node.nodes]
+#     for item in id_node_tup:
+#         trig = False
+#         for it in open_states:
+#             if it.id == item[0]:
+#                 trig = True
+#                 node.nodes.remove(item[1])
+#         for it in closed_states:
+#             if it.id == item[0] and trig is False:
+#                 node.nodes.remove(item[1])
